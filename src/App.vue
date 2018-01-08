835ed79d140a936b432e55346e7e152bb6e0abc5
xiaopeng <12405301@qq.com>
2018-01-08 19:26:15 +0800

    vue practice

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app">
+    <nav id="nav">
+      <ul>
+        <!-- Navigation Links -->
+        <li><router-link to="/">Home</router-link></li>
+        <li><router-link to="/admin">Admin</router-link></li>
+        <li><router-link to="/cart">Cart</router-link></li>
+      </ul>
+    </nav>
+    <br>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+#nav{
+  width: 100%;
+  height: 45px;
+  background-color: #000000;
+}
+
+#nav li{
+  display: inline-block;
+  width: 120px;
+  height: 45px;
+  line-height: 45px;
+  float: left;
+}
+
+#nav li a{
+  text-decoration: none;
+  color: white;
+  display: block;
+  text-align: center;
+  font-size: 20px;
+}
+
+#nav li a:hover{
+  background-color: #338ce6;
+}
+</style>
---
+++ src/pages/admin/Index.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <div class="admin-new">
+      <div class="container">
+        <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+          <ul class="admin-menu">
+            <!-- Links are to sibling routes -->
+            <li><router-link to="/admin">View Products</router-link></li>
+            <li><router-link to="/admin/new">New Product</router-link></li>
+          </ul>
+        </div>
+        <!-- Outlet for children routes -->
+        <router-view></router-view>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'home',
+    data () {
+      return {
+        msg: 'Welcome to the Admin Page'
+      }
+    }
+  }
+</script>
+
+<style>
+  .admin-menu {
+    list-style:none;
+    padding: 0px;
+    width: 150px;
+    height: auto;
+    float: left;
+    background: orange;
+  }
+
+
+  .admin-menu li{
+    width: 100%;
+    height: auto;
+    line-height: 45px;
+    border-top: white 2px solid;
+    float: left;
+  }
+
+  .admin-menu li a{
+    text-decoration: none;
+    color: black;
+    display: block;
+    text-align: center;
+    font-size: 20px;
+  }
+
+  .admin-menu li a:hover{
+    background-color: #338ce6;
+  }
+</style>
